db6694b2b0af10cfd2c4fe5e57ed8bb060e491e3
psostin <psostin@uc.cl>
2024-10-10 17:49:09 -0300

    Probemos

--- react-websocket-app/src/App.js
+++ react-websocket-app/src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 import React from 'react';
 import WebSocketComponent from './components/WebSocketComponent';
-import MapComponent from './components/MapComponent';
 
 const App = () => {
   return (
     <div className="App">
       
-      <h1>React WebSocket Application</h1>
+      <h1>Tarea 2 Pedro Sostin Ramirez</h1>
       <WebSocketComponent />
     </div>
   );
